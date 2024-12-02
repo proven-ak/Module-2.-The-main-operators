f598442a8919e084e798165f47b1569ff3cc18fb
proven_ak <proven@ya.ru>
2024-12-02 22:54:21 +0300

    02.12 Ввод числа в 1-е поле

--- module_2_6.py
+++ module_2_6.py
@@ -1,16 +1,15 @@
-# first_num = int(input("Введите число от 3 до 20"))
+first_num = int(input("Введите число от 3 до 20 : "))
 
-for first_num in range(3, 21):
-    pass_ = ""
-    pass_list = []
+result = ""
+pass_list = []
 
-    for i in range(1, 20):
-        for j in range(i, 20):
-            if i != j:
-                if first_num % (i + j) == 0:
-                    str_ = str(i) + str(j)
-                    pass_list.append(str_)
-                    pass_ = " ".join(pass_list)
+for i in range(1, 20):
+    for j in range(i, 20):
+        if i != j:
+            if first_num % (i + j) == 0:
+                str_ = str(i) + str(j)
+                pass_list.append(str_)
+                result = "".join(pass_list)
 
-    print(first_num, pass_)
+print(result)
 
